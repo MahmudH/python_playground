7da60e856190143af24c03039f30824a684dda22
mahmudh <mahmud.hussain@digital.cabinet-office.gov.uk>
2016-09-01 12:13:21 +0100

    working with lists

--- 3_if_statements.py
+++ 3_if_statements.py
@@ -1,4 +1,19 @@
-numbers = [0,1,2,3,4,5,6,7,8,9]
-for number in numbers:
-    if number > 5:
-        print number
+import random
+
+magic_numbers = [random.randint(0, 9), random.randint(0, 9)]
+
+def ask_user_and_check_number(chances):
+    user_input = int(input("Enter a number between 0 and 9, you have {} chances: ".format(chances)))
+    if user_input in magic_numbers:
+        return "You got the number right"
+    else:
+        return "You got the number wrong"
+
+def run_program_x_times(chances):
+    for attempt in range(chances):
+        print "This is attempt {}".format(attempt)
+        print ask_user_and_check_number(chances)
+    print "The random numbers were: {}".format(magic_numbers)
+
+run_program_x_times(5)
+
